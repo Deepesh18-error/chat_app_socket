c68eae04d49df6a89e9e96348da1ca97f8696c4f
DEEPESH SAXENA <deepeshsaxena18@gmail.com>
2025-05-31 22:35:27 +0530

    Added codes

--- index.js
+++ index.js
@@ -9,15 +9,12 @@ const io = socketio(server);
 io.on('connection' ,(socket) => {
     console.log("A User Connected" , socket.id); // every socket has a unique socket id if i do socket.id then we will get new id at every reload of web page localhost:3000
 
-    socket.on('from_client', () => {
-        console.log("event coming from client");
+    socket.on('msg_send', (data) => {
+        console.log(data);
+        // io.emit('msg_rcvd' , data);
+        // socket.emit('msg_rcvd' , data);
+            socket.broadcast.emit('msg_rcvd' , data);
     })
-
-
-    setInterval(() => {
-        socket.emit('from_server');
-    }, 2000);
-
 
 });
 
--- public/script.js
+++ public/script.js
@@ -1,14 +1,25 @@
 var socket = io();
 
 let btn = document.getElementById('btn');
-btn.onclick = function exec(){
-    socket.emit('from_client');
-}
+let inputMsg = document.getElementById('newmsg');
+let msgList = document.getElementById('msglist');
 
-socket.on('from_server', () => {
-    console.log("Collected a new event from server");
-    const div = document.createElement('div');
-    div.innerText = 'New event from server';
-    console.log(div);
-    document.body.appendChild(div);
+btn.onclick = function exec(){
+    socket.emit('msg_send' , {
+        msg: inputMsg.value 
+    });
+}
+// socket.on('from_server', () => {
+//     console.log("Collected a new event from server");
+//     const div = document.createElement('div');
+//     div.innerText = 'New event from server';
+//     console.log(div);
+//     document.body.appendChild(div);
+// });
+
+
+socket.on('msg_rcvd', (data) => {
+    const limsg = document.createElement('li');
+    limsg.innerText = data.msg;
+    msgList.appendChild(limsg);
 });
